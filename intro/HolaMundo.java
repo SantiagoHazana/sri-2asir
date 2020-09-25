6664108088ea582b63bc20210cb7b51a865785ca
Santiago Hazana <shazana@pc86-14.29006568.29.andared.ced.junta-andalucia.es>
2020-09-25 11:26:16 +0200

    Added scanner example

--- intro/HolaMundo.java
+++ intro/HolaMundo.java
@@ -2,9 +2,6 @@ package intro;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        Persona marina = new Persona();
-        marina.cambiarNombre("Marina");
-        marina.apellidos = "Moreno";
-        System.out.println(marina.dimeTuNombre());
+        System.out.println("Hola Mundo!");
     }
 }
--- intro/Persona.java
+++ intro/Persona.java
@@ -2,19 +2,54 @@ package intro;
 
 public class Persona {
 
-    private String nombre;
-    public String apellidos;
-    private int edad;
+    private String name;
+    private String surname;
+    private int age;
 
-    public String dimeTuNombre() {
-        return nombre + " " + apellidos;
+    public String getName() {
+        return name;
     }
 
-    public void cambiarNombre(String nuevoNombre) {
-        nombre = nuevoNombre;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int getEdad() {
-        return edad;
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean equals(Persona persona){
+        return this.name.equals(persona.name) && this.surname.equals(persona.surname) && this.age ==persona.age;
+    }
+
+    public static void main(String[] args) {
+        Persona santiago = new Persona();
+        Persona santiago2 = new Persona();
+        Persona marina = new Persona();
+        marina.setName("Marina");
+        marina.setSurname("Moreno");
+        marina.setAge(31);
+
+        santiago.setName("Santiago");
+        santiago.setSurname("Hazaña");
+        santiago.setAge(25);
+        santiago2.setName("Santiago");
+        santiago2.setSurname("Hazaña");
+        santiago2.setAge(25);
+        System.out.println(santiago.equals(marina));
+        System.out.println(santiago2.equals(santiago));
+        System.out.println("Mi nombre es " + santiago.getName() + " " + santiago.getSurname() + " y tengo " + santiago.getAge() + " años");
     }
 }
